bdb760a2f8ad5b2e8ec0bd068338a869315552d6
Kamsnyc <kamel.singh1999@gmail.com>
2023-10-31 22:01:14 -0400

    working on navigation bar

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <section className="bg-gray-400 h-14 flex items-center">
+      {/* COL 1 */}
+      <div className="">
+        {/* MENU BUTTON */}
+
+        {/* YOUTUBE LOGO */}
+      </div>
+      {/* COL 2 */}
+      <div className="">
+        {/* SEARCH */}
+        {/*   */}
+      </div>
+      {/* COL 3 */}
+      <div className="">
+        {/* Upload Video */}
+        {/* App Grid Icon */}
+        {/* NOTIFCATIONS ICON */}
+        {/* AVATAR */}
+      </div>
+    </section>
+  );
+}
+
+export default Navbar;
